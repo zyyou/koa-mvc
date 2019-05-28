cbb3a89b696f9a4b49a3f7abdb65784dedacbe5c
zyy <zhengyy@9186.com>
2019-05-28 11:01:48 +0800

    包更新

--- config/appconfig.js
+++ config/appconfig.js
@@ -1,7 +1,1 @@
-module.exports = {
-    tokenKey:'jwt_token_key_123456',    //jwt token key
-    tokenExpires:'30s', //jwt token 有效期
-    port: 3001,
-    title: 'koa mvc 模板',
-    author: 'zyy'
-}
+module.exports = require('./koamvc');
---
+++ config/koamvc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    tokenKey:'jwt_token_key_123456',    //jwt token key
+    tokenExpires:'30s', //jwt token 有效期
+    port: 3001,
+    title: 'koa mvc 模板',
+    author: 'zyy'
+}
